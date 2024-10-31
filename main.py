03b723c153f48609d4a2629a492a5394ef2d5d79
ichyy-n <yuichiron618@icloud.com>
2024-10-31 23:46:46 +0900

    commit

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ async def handle_callback(request: Request, db: Session = Depends(get_db)):
     
     global user_id
     user_id = get_user_id(events)
-    token = db.get(User, 1)
+    token = db.get(User, 1).token
     print(type(token))
     #tokenがないならGoogle認証用urlを送信
     if token is None:
